5d4ec0391c690211378c92ecaf798c832199868b
Max P <Mail@MPassarello.de>
2024-03-10 15:05:25 +0100

    Enhance AsyncFIFO with Gray code counters
    Refactored the Asynchronous FIFO module to employ Gray code counters for read and write pointer tracking, ensuring reliable pointer comparison across clock domains. Updated module naming to 'AsyncFIFO' and improved signal naming for clarity. Enhanced documentation with in-line comments describing interface signals and process functionality. Incorporated a new process ("Flags") to determine 'FullFlag' and 'EmptyFlag' based on pointer values. Added a 'GrayCounter' component to simplify pointer management and avoid direct binary-to-Gray and Gray-to-binary conversions in the read and write processes. The write and read processes are now triggering on rising edges of independent clocks and are conditionally enabled to ensure proper synchronization. Removed deprecated components and flattened signal assignments for better readability and maintainability. Set attribute 'RAM_STYLE' for FIFO memory to facilitate technology-specific synthesis optimizations.

--- code/AsyncFIFO.vhd
+++ code/AsyncFIFO.vhd
@@ -1,66 +1,78 @@
 ----------------------------------------------------------------------------------
--- @Name 	FIFO2CLK
--- @Version	1.1.0
--- @Author	Maximilian Passarello
--- @E-Mail	atom-dragon@gmx.net
+-- @name        Asynchronous FIFO
+-- @version     1.1.1
+-- @author      Maximilian Passarello (mpassarello.de)
+--@             Asynchronous FIFO with Gray Code Adress
+-- @history
+-- - 1.1.0 (2009-05-16) Initial version
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
-entity FIFO2CLK is
+entity AsyncFIFO is
     generic (
-        Width       : integer := 32;             --Wort Breite
-        Depth       : integer := 4;              --FIFO Adress Breite
-        RamTypeFifo : string  := "Distributed"); --Ram Typ des Fifos | Block or Distributed |
+        --@ FIFO Word width: Data width of the FIFO
+        Width : integer := 32;
+        --@ FIFO depth: Number of words the FIFO can store
+        Depth : integer := 4;
+        --@ Implementation of the RAM: "Block" or "Distributed"
+        RamTypeFifo : string := "Distributed"
+    );
     port (
-        CLKwrite : in std_logic;
-        CLKread  : in std_logic;
-        --CE : In std_logic;
-        RST   : in std_logic;
-        Din   : in std_logic_vector(Width - 1 downto 0);
-        Dout  : out std_logic_vector(Width - 1 downto 0);
-        RE    : in std_logic;
-        WE    : in std_logic;
-        Empty : out std_logic;
-        Full  : out std_logic);
-end FIFO2CLK;
+        --@ @virtualbus Write-Interface @dir in FIFO write interface
+        --@ Write clock; indipendent from the read clock. **Rising edge sensitive**
+        WriteCLK : in std_logic;
+        --@ Write reset; synchronous reset. **Active high**
+        WriteRST : in std_logic;
+        --@ Write clock enable: Used for the `Write`- and `WriteGrayCounter`-process. **Active high**
+        WriteCE : in std_logic;
+        --@ Data input: Must be valid at the rising edge of the write clock if the write enable signal is set.
+        DataIn : in std_logic_vector(Width - 1 downto 0);
+        --@ Enable the write of the data to the FIFO, if the FIFO is not full. **Active high**
+        WriteEnable : in std_logic;
+        --@ Full flag: Indicates if the FIFO is full. **Active high**
+        FullFlag : out std_logic;
+        --@ @end
+        --@ @virtualbus Read-Interface @dir out FIFO read interface
+        --@ Read clock; indipendent from the write clock. **Rising edge sensitive**
+        ReadCLK : in std_logic;
+        --@ Read reset; synchronous reset. **Active high**
+        ReadRST : in std_logic;
+        --@ Read clock enable: Used for the `Read`- and `ReadGrayCounter`-process. **Active high**
+        ReadCE : in std_logic;
+        --@ Data output: The data is valid at the rising edge of the read clock
+        --@ one cycle after the read enable signal is set.
+        DataOut : out std_logic_vector(Width - 1 downto 0);
+        --@ Enable the read of the data from the FIFO, if the FIFO is not empty. **Active high**
+        ReadEnable : in std_logic;
+        --@ Empty flag: Indicates if the FIFO is empty. **Active high**
+        EmptyFlag : out std_logic
+        --@ @end
+    );
+end AsyncFIFO;
 
---	component FIFO2CLK
---		generic(	Width : integer := 32;
---					Depth : integer := 32;
---					RamTypeFifo : string := "Distributed");
---		port(	CLKwrite : In std_logic;
---				CLKread : In std_logic;
---				--CE : In std_logic;
---				RST : In std_logic;
---				Din : In std_logic_vector(Width-1 downto 0);
---				Dout : Out std_logic_vector(Width-1 downto 0);
---				RE : In std_logic;
---				WE : In std_logic;
---				Empty : Out std_logic;
---				Full : Out std_logic);
---	end component;
---
---	FIFO2CLK0: FIFO2CLK
---		generic map(Width => 32,
---						Depth => 32,
---						RamTypeFifo => "Distributed")
---		port map(CLKwrite => ,
---					CLKread => ,
---					--CE => ,
---					RST => ,
---					Din => ,
---					Dout => ,
---					RE => ,
---					WE => ,
---					Empty => ,
---					Full => );
+architecture Behavioral of AsyncFIFO is
 
-architecture Behavioral of FIFO2CLK is
+    component GrayCounter
+        generic (
+            Width             : integer;
+            InitialValue      : integer;
+            ResetValue        : integer;
+            CountingDirection : string;
+            LookAhead         : integer
+        );
+        port (
+            CLK                       : in std_logic;
+            CE                        : in std_logic;
+            RST                       : in std_logic;
+            CountEnable               : in std_logic;
+            GrayCounterValue          : out std_logic_vector(Width - 1 downto 0);
+            GrayCounterLookAheadValue : out std_logic_vector(Width - 1 downto 0)
+        );
+    end component;
 
-    signal CE : std_logic := '1';
-
+    --@ Calculate the log2 of a number
     function log2(N : integer) return integer is
     begin
         if (N <= 2) then
@@ -74,122 +86,151 @@ architecture Behavioral of FIFO2CLK is
         end if;
     end function log2;
 
-    function GrayToBinary(Bin : std_logic_vector) return std_logic_vector is
-        constant Width            : integer := Bin'Length;
-        variable Temp             : std_logic_vector(Width - 1 downto 0);
-    begin
-        Temp(Width - 1) := Bin(Width - 1);
-        for i in 1 to Width - 1 loop
-            Temp((Width - 1) - i) := Temp(Width - i) xor Bin((Width - 1) - i);
-        end loop;
-        return Temp(Width - 1 downto 0);
-    end function GrayToBinary;
-
-    --	function BinaryToGray(Bin : std_logic_vector) return std_logic_vector is
-    --		constant Width : integer := Bin'Length;
-    --		variable Temp : std_logic_vector(Width downto 0);
-    --	begin
-    --		Temp := std_logic_vector(unsigned('0' & (Bin));
-    --		Temp := (Temp(Width-1 downto 0) & '0') XOR ('0' & Temp(Width-1 downto 0));
-    --		Temp := '0' & Temp(Width downto 1);
-    --		Temp := '0' & Temp(Width-1 downto 0);
-    --		return Temp(Width-1 downto 0);
-    --	end function BinaryToGray;
-
+    --@ FIFO memory address width:
+    --@ The address width is calculated from the depth of the FIFO.
     constant AdressWidth : integer := log2(Depth);
 
-    attribute RAM_STYLE : string;
-
+    --@ FIFO memory type: `Depth` x `Width`
     type FifoType is array(Depth - 1 downto 0)
     of std_logic_vector(Width - 1 downto 0);
-    signal Fifo : FifoType;
-
+    --@ FIFO memory
+    --@ The FIFO memory is implemented as a RAM with the specified `RamTypeFifo`:
+    --@ "Block" or "Distributed".
+    signal Fifo                 : FifoType;
+    attribute RAM_STYLE         : string;
     attribute RAM_STYLE of Fifo : signal is RamTypeFifo;
 
-    signal RCount    : std_logic_vector(AdressWidth - 1 downto 0);
-    signal RCountBB  : std_logic_vector(AdressWidth - 1 downto 0);
-    signal RCountOut : std_logic_vector(AdressWidth - 1 downto 0);
-    signal RCountIn0 : std_logic_vector(AdressWidth - 1 downto 0);
-    signal RCountIn1 : std_logic_vector(AdressWidth - 1 downto 0);
-    signal WCount    : std_logic_vector(AdressWidth - 1 downto 0);
-    signal WCountBB  : std_logic_vector(AdressWidth - 1 downto 0);
-    signal WCountOut : std_logic_vector(AdressWidth - 1 downto 0);
-    signal WCountIn0 : std_logic_vector(AdressWidth - 1 downto 0);
-    signal WCountIn1 : std_logic_vector(AdressWidth - 1 downto 0);
-
-    signal FifoFull  : std_logic;
+    --@ Increment enable signal for the read pointer.
+    signal ReadCounterEnable : std_logic;
+    --@ Internal empty flag; forwarded to the output.
     signal FifoEmpty : std_logic;
+    --@ Internal read pointer.
+    signal ReadPointer : std_logic_vector(AdressWidth - 1 downto 0);
+    --@ Internal read pointer look ahead. (Read pointer + 1)
+    signal ReadPointerLookAhead : std_logic_vector(AdressWidth - 1 downto 0);
+
+    --@ Increment enable signal for the write pointer.
+    signal WriteCounterEnable : std_logic;
+    --@ Internal full flag; forwarded to the output.
+    signal FifoFull : std_logic;
+    --@ Internal write pointer.
+    signal WritePointer : std_logic_vector(AdressWidth - 1 downto 0);
+    --@ Internal write pointer look ahead. (Write pointer + 1)
+    signal WritePointerLookAhead : std_logic_vector(AdressWidth - 1 downto 0);
 
 begin
 
-    Full  <= FifoFull;
-    Empty <= FifoEmpty;
+    FullFlag  <= FifoFull;
+    EmptyFlag <= FifoEmpty;
 
-    process (CLKwrite)
+    --@ Full and empty flags
+    --@ 
+    --@ The full and empty flags are purely combinatorial calculated
+    --@ from a comparison of the read and write pointers/look ahead values.
+    Flags : process (WritePointer, ReadPointer, WritePointerLookAhead)
+    begin
+        if ReadPointer = WritePointerLookAhead then
+            FifoFull <= '1';
+        else
+            FifoFull <= '0';
+        end if;
+        if ReadPointer = WritePointer then
+            FifoEmpty <= '1';
+        else
+            FifoEmpty <= '0';
+        end if;
+    end process;
+
+    --@ Write pointer as Gray counter
+    --@ 
+    --@ The write pointer is incremented by one from the `Write`-process
+    --@ if the FIFO is not full and the write enable signal is set.
+    --@ The look ahead value is used as the next write pointer value and
+    --@ to check if the FIFO is full.
+    WriteGrayCounter : GrayCounter
+    generic map(
+        Width             => AdressWidth,
+        InitialValue      => 0,
+        ResetValue        => 0,
+        CountingDirection => "UP",
+        LookAhead         => 1
+    )
+    port map(
+        CLK                       => WriteCLK,
+        CE                        => WriteCE,
+        RST                       => WriteRST,
+        CountEnable               => WriteCounterEnable,
+        GrayCounterValue          => WritePointer,
+        GrayCounterLookAheadValue => WritePointerLookAhead
+    );
+
+    --@ Write process
+    --@ 
+    --@ The write process writes the data to the FIFO if the FIFO is not full
+    --@ and the write enable signal is set.
+    --@ `WriteCountEnable` is used to synchronize the write pointer increment
+    --@ and is reset every time a rising edge of the write clock is detected.
+    Write : process (WriteCLK)
         variable TempW : std_logic_vector(AdressWidth downto 0);
     begin
-        if rising_edge(CLKwrite) then
-            if RST = '1' then
-                WCount    <= (others => '0');
-                WCountOut <= (others => '0');
-                RCountIn0 <= (others => '0');
-                RCountIn1 <= (others => '0');
-                FifoFull  <= '0';
-            elsif CE = '1' then
-                RCountIn0 <= RCountOut;
-                RCountIn1 <= RCountIn0;
-                RCountBB  <= GrayToBinary(RCountIn1);
-                if GrayToBinary(RCountIn1) = std_logic_vector(unsigned(WCount) + 1) then
-                    FifoFull <= '1';
-                else
-                    FifoFull <= '0';
-                end if;
-                if WE = '1' then
-                    if GrayToBinary(RCountIn1) /= std_logic_vector(unsigned(WCount) + 1) then
-                        TempW := std_logic_vector(unsigned('0' & WCount) + 1);
-                        TempW := (TempW(AdressWidth - 1 downto 0) & '0') xor ('0' & TempW(AdressWidth - 1 downto 0));
-                        TempW := '0' & TempW(AdressWidth downto 1);
-                        TempW := '0' & TempW(AdressWidth - 1 downto 0);
-                        WCountOut                              <= TempW(AdressWidth - 1 downto 0);
-                        WCount                                 <= std_logic_vector(unsigned(WCount) + 1);
-                        Fifo(to_integer(unsigned(WCount) + 1)) <= Din;
+        if rising_edge(WriteCLK) then
+            if WriteRST = '1' then
+                WriteCounterEnable <= '0';
+            elsif WriteCE = '1' then
+                WriteCounterEnable <= '0';
+
+                if WriteEnable = '1' then
+                    if FifoFull = '0' then
+                        Fifo(to_integer(unsigned(WritePointerLookAhead))) <= DataIn;
+                        WriteCounterEnable                                <= '1';
                     end if;
                 end if;
             end if;
         end if;
     end process;
 
-    process (CLKread)
+    --@ Read pointer as Gray counter
+    --@ 
+    --@ The read pointer is incremented by one from the `Read`-process
+    --@ if the FIFO is not empty and the read enable signal is set.
+    --@ 
+    --@ The look ahead value is used as the next read pointer value.
+    ReadGrayCounter : GrayCounter
+    generic map(
+        Width             => AdressWidth,
+        InitialValue      => 0,
+        ResetValue        => 0,
+        CountingDirection => "UP",
+        LookAhead         => 1
+    )
+    port map(
+        CLK                       => ReadCLK,
+        CE                        => ReadCE,
+        RST                       => ReadRST,
+        CountEnable               => ReadCounterEnable,
+        GrayCounterValue          => ReadPointer,
+        GrayCounterLookAheadValue => ReadPointerLookAhead
+    );
+
+    --@ Read process
+    --@ 
+    --@ The read process reads the data from the FIFO if the FIFO is not empty
+    --@ and the read enable signal is set.
+    --@ `ReadCountEnable` is used to synchronize the read pointer increment
+    --@ and is reset every time a rising edge of the read clock is detected.
+    Read : process (ReadCLK)
         variable TempR : std_logic_vector(AdressWidth downto 0);
     begin
-        if rising_edge(CLKread) then
-            if RST = '1' then
-                RCount    <= (others => '0');
-                RCountOut <= (others => '0');
-                WCountIn0 <= (others => '0');
-                WCountIn1 <= (others => '0');
-                Dout      <= (others => '0');
-                FifoEmpty <= '1';
-            elsif CE = '1' then
-                WCountIn0 <= WCountOut;
-                WCountIn1 <= WCountIn0;
-                WCountBB  <= GrayToBinary(WCountIn1);
-                if GrayToBinary(WCountIn1) = RCount then
-                    FifoEmpty <= '1';
-                else
-                    FifoEmpty <= '0';
-                end if;
-                if RE = '1' then
-                    if GrayToBinary(WCountIn1) /= RCount then
-                        TempR := std_logic_vector(unsigned('0' & RCount) + 1);
-                        TempR := (TempR(AdressWidth - 1 downto 0) & '0') xor ('0' & TempR(AdressWidth - 1 downto 0));
-                        TempR := '0' & TempR(AdressWidth downto 1);
-                        TempR := '0' & TempR(AdressWidth - 1 downto 0);
-                        RCountOut <= TempR(AdressWidth - 1 downto 0);
-                        RCount    <= std_logic_vector(unsigned(RCount) + 1);
-                        Dout      <= Fifo(to_integer(unsigned(RCount) + 1));
-                    else
-                        Dout <= (others => '0');
+        if rising_edge(ReadCLK) then
+            if ReadRST = '1' then
+                ReadCounterEnable <= '0';
+            elsif ReadCE = '1' then
+                ReadCounterEnable <= '0';
+
+                if ReadEnable = '1' then
+                    if FifoEmpty = '0' then
+                        DataOut           <= Fifo(to_integer(unsigned(ReadPointerLookAhead)));
+                        ReadCounterEnable <= '1';
                     end if;
                 end if;
             end if;
